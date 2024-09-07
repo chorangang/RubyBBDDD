b3e1c1f36bceeebce1f2f9c2e435e9c5a940eaa2
chorangang <choransemail@gmail.com>
2024-09-08 04:03:21 +0900

    [wip]日記など編集。テストやる時間なさそうなので諦める

--- src/routes/routes.rb
+++ src/routes/routes.rb
@@ -22,9 +22,9 @@ ROUTES = [
     { method: 'DELETE', path: '/api/threads/:id', controller: 'ThreadsController', action: :destroy },
 
     # Comments
-    { method: 'GET',    path: '/api/threads/:id/comments',     controller: 'CommentsController', action: :index },
-    { method: 'POST',   path: '/api/comments',     controller: 'CommentsController', action: :create },
-    { method: 'GET',    path: '/api/comments/:id', controller: 'CommentsController', action: :show },
-    { method: 'PUT',    path: '/api/comments/:id', controller: 'CommentsController', action: :update },
-    { method: 'DELETE', path: '/api/comments/:id', controller: 'CommentsController', action: :destroy },
+    { method: 'GET',    path: '/api/threads/:id/comments', controller: 'CommentsController', action: :index },
+    { method: 'POST',   path: '/api/comments',             controller: 'CommentsController', action: :create },
+    { method: 'GET',    path: '/api/comments/:id',         controller: 'CommentsController', action: :show },
+    { method: 'PUT',    path: '/api/comments/:id',         controller: 'CommentsController', action: :update },
+    { method: 'DELETE', path: '/api/comments/:id',         controller: 'CommentsController', action: :destroy },
 ]
